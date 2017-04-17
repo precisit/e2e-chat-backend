2fba31d757eae52d367e62a0b11a293c47e5b703
godiagonal <same_johansson@hotmail.com>
2017-04-17 15:06:10 +0200

    Added comments and updated code to match tutorial

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ var moment = require('moment');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
 var port = 3001;
 
+// Start the HTTP server
+http.listen(port, function(){
+  console.log('Server started on port: ' + port);
+});
+
+// Start listening to socket connections
 io.on('connection', function(socket) {
   console.log('New user connected: ' + socket.id);
 
@@ -12,18 +17,23 @@ io.on('connection', function(socket) {
     console.log('User disconnected: ' + socket.id);
   });
 
-  socket.emit('messageToClient', {
+  // Send a welcome message to the client
+  socket.emit('newMessage', {
     time: moment().format('HH:mm:ss'),
     text: 'Welcome to the chat!'
   });
 
-  socket.on('messageToServer', function(message) {
-    message.time = moment().format('HH:mm:ss');
-    console.log(message);
-    io.sockets.emit('messageToClient', message);
-  });
-});
+  // When a new message is received from the client...
+  socket.on('newMessage', function(messageText) {
+    // ... add the current time...
+    var message = {
+      time: moment().format('HH:mm:ss'),
+      text: messageText
+    };
 
-http.listen(port, function(){
-  console.log('Server started on port: ' + port);
+    console.log(message);
+
+    // ... and forward it to all connected clients
+    io.sockets.emit('newMessage', message);
+  });
 });
